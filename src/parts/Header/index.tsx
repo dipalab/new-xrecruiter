8f859e39de906bea651506342a2d66cf177afb20
MamadFarid <mamadfarid2105@gmail.com>
2023-06-21 11:48:07 +0700

    update mobile menu

--- src/parts/Header/index.tsx
+++ src/parts/Header/index.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
-import { Disclosure } from '@headlessui/react'
+import { Fragment } from 'react'
+import { Disclosure, Transition } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Link, useLocation } from 'react-router-dom'
 import { Button, Logo } from '../../components'
@@ -27,7 +28,7 @@ const Header = ({ headerTheme }: IHeader) => {
 
   return (
     <>
-      <Disclosure as="nav" className="w-full h-[64px] md:h-20 xl:h-[90px] absolute top-0 z-10 bg-transparent">
+      <Disclosure as="nav" className="w-full h-[64px] md:h-20 xl:h-[90px] absolute top-0 z-50 bg-transparent">
         {({ open }) => (
           <>
             <div className="relative w-full h-full">
@@ -73,34 +74,69 @@ const Header = ({ headerTheme }: IHeader) => {
                       headerTheme === HeaderTheme.DARK ? 'text-white' : 'text-neutral-100',
                       'inline-flex items-center justify-center rounded-md focus:outline-none focus:ring-0 focus:ring-inset'
                     )}>
-                      {open
-                        ? <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                        : <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                      }
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                     </Disclosure.Button>
                   </div>
                 </div>
               </div>
             </div>
 
-            <Disclosure.Panel className="sm:hidden">
-              <div className="bg-white">
-                <div className="space-y-1 px-2 pb-3 pt-2">
-                  {NavigationList.map((item) => (
-                    <Link
-                      key={item.name}
-                      to={item.to}
-                      className={clsx(
-                        item.to === currentLocation ? 'font-cera-pro-medium' : 'font-cera-pro-regular',
-                        'block rounded-md px-3 py-2 text-base text-gray-800'
-                      )}
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            </Disclosure.Panel>
+            <Transition appear show={open} as={Fragment}>
+              <Disclosure.Panel className="sm:hidden">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6">
+                    <div className="flex items-center justify-between h-16">
+                      <Link to="/" className="block h-7 w-auto">
+                        <Logo headerTheme={HeaderTheme.LIGHT} />
+                      </Link>
+
+                      <Disclosure.Button>
+                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                      </Disclosure.Button>
+                    </div>
+
+                    <div className="mt-20 flow-root">
+                      <div className="divide-y divide-gray-500/10">
+                        <div className="space-y-10 text-center">
+                          {NavigationList.map((item) => (
+                            <Link
+                              key={item.name}
+                              to={item.to}
+                              className={clsx(
+                                item.to === currentLocation ? 'font-cera-pro-medium text-blue-primary-main' : 'font-cera-pro-regular text-gray-800',
+                                'block rounded-md text-base'
+                              )}
+                            >
+                              {item.name}
+                            </Link>
+                          ))}
+
+                          <div>
+                            <Link to={''}>
+                              <Button classNames="!px-6 !py-[13px] !text-base !leading-5 md:!text-base md:!leading-5 !font-cera-pro-medium">Apply Now</Button>
+                            </Link>
+                          </div>
+
+                          <div>
+                            <Link to={''} className="text-base font-cera-pro-medium underline text-neutral-100">
+                            Login
+                            </Link>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Transition.Child>
+              </Disclosure.Panel>
+            </Transition>
           </>
         )}
       </Disclosure>
